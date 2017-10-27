4e03f350959d063785e90a2e56b55a3e9bfebcfb
Takao Chiba <takao.chiba@gmail.com>
2017-10-27 14:59:12 +0900

    Parse comma separated number

--- reviewtoslack/playstore/rating_client.py
+++ reviewtoslack/playstore/rating_client.py
@@ -15,10 +15,10 @@ def load_rating(package_name):
     rating.rating_value = float(tree.xpath('//*[@class="score-container"]/*[@itemprop="ratingValue"]/@content')[0])
     rating.rating_count = int(tree.xpath('//*[@class="score-container"]/*[@itemprop="ratingCount"]/@content')[0])
 
-    rating.star_five = int(tree.xpath('//*[@class="rating-bar-container five"]/*[@class="bar-number"]/text()')[0])
-    rating.star_four = int(tree.xpath('//*[@class="rating-bar-container four"]/*[@class="bar-number"]/text()')[0])
-    rating.star_three = int(tree.xpath('//*[@class="rating-bar-container three"]/*[@class="bar-number"]/text()')[0])
-    rating.star_two = int(tree.xpath('//*[@class="rating-bar-container two"]/*[@class="bar-number"]/text()')[0])
-    rating.star_one = int(tree.xpath('//*[@class="rating-bar-container one"]/*[@class="bar-number"]/text()')[0])
+    rating.star_five = int(tree.xpath('//*[@class="rating-bar-container five"]/*[@class="bar-number"]/text()')[0].replace(',', ''))
+    rating.star_four = int(tree.xpath('//*[@class="rating-bar-container four"]/*[@class="bar-number"]/text()')[0].replace(',', ''))
+    rating.star_three = int(tree.xpath('//*[@class="rating-bar-container three"]/*[@class="bar-number"]/text()')[0].replace(',', ''))
+    rating.star_two = int(tree.xpath('//*[@class="rating-bar-container two"]/*[@class="bar-number"]/text()')[0].replace(',', ''))
+    rating.star_one = int(tree.xpath('//*[@class="rating-bar-container one"]/*[@class="bar-number"]/text()')[0].replace(',', ''))
 
     return rating
